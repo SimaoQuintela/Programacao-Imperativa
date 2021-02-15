2b1e6d30c6a2aafef47fdb86de9457a9323b2158
Simão Quintela <71183501+SimaoQuintela@users.noreply.github.com>
2021-02-15 22:22:01 +0000

    Add files via upload

---
+++ Self Learning/ex-10.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main() {
+
+    // declarando variáveis
+    int numero, a, b, c; // a b c são 3 números consecutivos
+
+    printf("Insira um numero a sua escolha: ");
+    scanf("%d", &numero);
+
+    a = 1;
+    b = 2;
+    c = 3;
+
+    while (a * b * c <= numero) {
+        if (a * b * c == numero) {
+            printf("O numero %d e' triangular pois %d x %d x %d = %d\n", numero, a, b, c, numero);
+            a++;
+            b++;
+            c++;
+        } else {
+            a++;
+            b++;
+            c++;
+        }
+    }
+    a--;
+    b--;
+    c--;
+
+    if ( a * b * c < numero) {
+        printf("O numero %d nao e' triangular\n", numero);
+    }
+
+    system("pause");
+}
