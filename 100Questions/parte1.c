c086466732931e37c2fbc330b02ebb609cc6b7bb
Simão Quintela <71183501+SimaoQuintela@users.noreply.github.com>
2021-03-07 16:10:35 +0000

    Update parte1.c

--- 100Questions/parte1.c
+++ 100Questions/parte1.c
@@ -145,6 +145,7 @@ int mystrcmp(char s1[], char s2[]) {
 }
 
 //10 a function that returns the position where a string occures in other string 
+    // problems with the type of the return
 
 char* mystrstr(char s1[], char s2[]){
     size_t i = 0, j = 0;    // I used this type because I need to compare i and j with a strlen function which type is size_t, so I used it on i and j to match the types. In this case
